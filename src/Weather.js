d7ccd524593714715681d780df908ab1e52456ca
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2022-11-19 15:47:04 -0500

    changed color of loader to red

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ export default function Weather(props) {
     return <Triangle
     height="80"
     width="80"
-    color="#4fa94d"
+    color="red"
     ariaLabel="triangle-loading"
     wrapperStyle={{}}
     wrapperClassName=""
